77f51953f80356cc70fba13732185403bdf8387e
Benji <benji.lo@krakentechnologies.ltd>
2023-04-19 13:53:21 +0100

    styling

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-- <a href="https://seddonym.me/2020/11/19/trouble-atomic/" target="_blank">Djangos `transaction.atomic` context manager can lead to unexpected behavior when dealing with database transactions</a> on April 17th 2023
+
+* [Djangos `transaction.atomic` context manager can lead to unexpected behavior when dealing with database transactions](https://seddonym.me/2020/11/19/trouble-atomic) On April 19th 2023
+* [Not to union Python list types](https://til.codeinthehole.com/posts/not-to-union-python-list-types/) On April 19th 2023
--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 import { marked } from 'marked';
 
 async function fetchReadme() {
-  const response = await fetch(
-    'https://api.github.com/repos/benji011/TIL/contents/README.md');
-  const data = await response.json();
-  const markdown = atob(data.content);
-  const html = marked(markdown);
-  document.getElementById('readme').innerHTML = html;
+  const response = await fetch('README.md');
+  const markdown = await response.text();
+  const html = marked(markdown).replace(/<a /g, '<a target="_blank" ');
+  document.getElementById('readme').innerHTML = html
 }
 
 fetchReadme();
