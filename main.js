285c57425d3fd818b4ba056a71bb404d2671f6a5
Benji <benji.lo@krakentechnologies.ltd>
2023-04-21 11:55:06 +0100

    just show the README from a public repo

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { marked } from 'marked';
 
 async function fetchReadme() {
-  const response = await fetch('https://gist.githubusercontent.com/benji011/1e6950e8af13893b9fcf03f0d74e9b31/raw/c0546e196aa97676f119fc16abe3344fd53c4bb3/test.md');
+  const response = await fetch('https://raw.githubusercontent.com/benji011/TIL_notes/main/README.md');
   const markdown = await response.text();
   const html = marked(markdown).replace(/<a /g, '<a target="_blank" ');
   document.getElementById('readme').innerHTML = html
